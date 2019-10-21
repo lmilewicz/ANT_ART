9950d6c024710051fdc99028ff1fdf348c3967e0
lmilewicz <luke21sc@gmail.com>
2019-10-21 19:54:44 +0200

    Added ACC_VI.py

---
+++ ACC_VI.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
+######  Step 0  ######
+
+# Maximum number of ants:
+ant_number = 5
+
+# Maximum number of iterations:
+Mn = 10
+
+# Side length of local region:
+s = 5
+
+# Maximum speed of ants moving
+v_max = 10
+
+# Alpha & betha parameters
+alpha = 1 
+betha = 1
+
+######  Step 1  ######
+#Project the data onthe plane - give a pair of coordinate (x,y) to each object randomly
+#Each ant that is currently unloeaded chooses an object at random
+
+
+######  Step 2  ######
+
+#for i in range(1, Mn):
+#    for j in range(1, ant_number):
+        #2.1
+        #2.2 
+        #2.3
+        
+
+######  Step 3  ######
+
