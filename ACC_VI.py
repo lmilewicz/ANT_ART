2c27654a6814e52cc7885363ab2b66ac68ff747f
lmilewicz <luke21sc@gmail.com>
2019-11-05 00:11:42 +0100

    Adding CDbw functions

--- ACC_VI.py
+++ ACC_VI.py
@@ -3,8 +3,7 @@ import numpy as np
 import random
 import math
 
-from numpy import (array, dot, arccos, clip)
-from numpy.linalg import norm
+from CDbw import CDbw
 
 ######  Step 0  ######
 
@@ -20,9 +19,9 @@ s = 5
 # Maximum speed of ants moving
 v_max = 10
 
-# Alpha & betha parameters
+# Alpha & beta parameters
 alpha = 0.8 
-betha = 0.9
+beta = 0.9
 
 # Quantity of objects
 N = 100
@@ -43,61 +42,60 @@ s = 10
 v = 34
 vmax = 34
 
-# function
-def sigmoid_function(x,betha):
-  return 1 / (1+ math.exp(- x * betha))
+# functions
+def sigmoid_function(x, beta):
+    return 1 / (1+ math.exp(-x * beta))
 
+def sim(o_i, o_j):
+    return np.sum(o_i*o_j)/np.sqrt(np.sum(o_i*o_i)*np.sum(o_j*o_j))
 
 ######  Step 1  ######
 #Project the data onthe plane - give a pair of coordinate (x,y) to each object randomly
 #Each ant that is currently unloeaded chooses an object at random
-for object_index in range(N):
+for i in range(N):
     x = random.randint(0,X-1)
     y = random.randint(0,Y-1)
     space[x,y]=1
 
+objects = np.transpose(np.nonzero(space))
 
-[nonzero_x,nonzero_y] = np.nonzero(space)
-print(nonzero_x)
-print(nonzero_y)
+U = []
+U.append(objects[0:5])
+U.append(objects[5:10])
+
+print(CDbw(U))
+
+
 
 ######  Step 2  ######
 
 for i in range(1, Mn):
     for j in range(1, ant_number):
-        for n in range(N-2):
-            print(i,j,n)
-            oix = nonzero_x[n]
-            oiy = nonzero_y[n]
+        for oixy in objects:
+            #print(i,j,n)
+            oix = oixy[0]
+            oiy = oixy[1]
             mask_min_x=max(0,oix-s)
             mask_min_y=max(0,oiy-s)
             mask_max_x=min(oix+s,X)
             mask_max_y=min(oiy+s,Y)
-##            print(oix,oiy,'sd')
-##            print(mask_min_x)
-##            print(mask_min_y)
-##            print(mask_max_x)
-##            print(mask_max_y)
+
             neigh_sum = 0
-            for n2 in range(N-2):
-                ojx = nonzero_x[n2]
-                ojy = nonzero_y[n2]                  
-                if ((ojx != oix) or (oiy != ojy)) and (ojx > mask_min_x) and (ojx < mask_max_x) and (ojy > mask_min_y) and (ojy < mask_max_y) :
-                    object=[oix,oiy]
-                    neighbour=[ojx,ojy]
-##                    print(neighbour)
-                    doioj = dot(object,neighbour)/norm(object)/norm(neighbour)
-##                    print(foi)
-                    neigh_sum = neigh_sum + (1-(doioj/(alpha*(1+((v-1)/vmax)))))
-            foi = max(0,neigh_sum/(s*s))
-            sigm_fun = sigmoid_function(foi,betha)
+            for ojxy in objects:
+                ojx = ojxy[0]
+                ojy = ojxy[1]                  
+                if (oix != ojx or oiy != ojy) and ojx > mask_min_x and ojx < mask_max_x and ojy > mask_min_y and ojy < mask_max_y :
+                    neigh_sum = neigh_sum + (1-(1-sim(oixy,ojxy)/(alpha*(1+((v-1)/vmax)))))
+
+            foi = max(0, neigh_sum/(s*s))
+            sigm_fun = sigmoid_function(foi,beta)
             rand=random.random()
-            picking_prob = 1 - sigmoid_function(foi,betha)
+            picking_prob = 1 - sigmoid_function(foi,beta)
             if (picking_prob > rand) and space[oix,oiy] != 2 :
                 space[oix,oiy] = 2
 
-            droping_prob = sigmoid_function(foi,betha)
-            print(sigm_fun)
+            droping_prob = sigmoid_function(foi,beta)
+            #print(sigm_fun)
         
 
 ######  Step 3  ######
--- CDbw.py
+++ CDbw.py
@@ -2,3 +2,64 @@
 
 #Clustering Validity Index
 
+import numpy as np
+
+def density(u, U):
+    out = 0
+    U_stdDev = np.std(U)
+    for u_i in U:
+        if np.linalg.norm(u_i - u) <= U_stdDev:
+            out = out + 1
+    return out
+  
+def densityZ(z_ij, U_i, U_j):
+    out = 0
+    U_ij_stdDev = (np.std(U_i)+np.std(U_j))/2
+    for u_i in U_i:
+        if np.linalg.norm(u_i - z_ij) <= U_ij_stdDev:
+            out = out + 1
+    for u_j in U_j:
+        if np.linalg.norm(u_j - z_ij) <= U_ij_stdDev:
+            out = out + 1
+    return out/(len(U_i)+len(U_j))
+
+def closeRepCal(U_i, U_j):
+    close_rep_dist = 99999
+    rep_i_min = rep_j_min = [0, 0]
+    for rep_i in U_i:
+        for rep_j in U_j:
+            if close_rep_dist > np.linalg.norm(U_i-U_j):
+                close_rep_dist = np.linalg.norm(U_i-U_j)
+                rep_i_min = rep_i
+                rep_j_min = rep_j
+    return close_rep_dist, rep_i_min, rep_j_min
+  
+def Intra_den(U):
+    out = 0
+    for U_i in U:
+        for u_ij in U_i:
+            out = out + density(u_ij, U_i)
+        out = out/len(U_i)
+    return out/len(U)
+
+def Inter_den(U):
+    out = 0
+    for i in range(len(U)):
+        for j in range(len(U)):
+            if i != j:
+                close_rep_dist, rep_i_min, rep_j_min = closeRepCal(U[i], U[j])
+                z_ij = [np.round((rep_i_min[0]+rep_j_min[0]/2)), np.round((rep_i_min[0]+rep_j_min[0])/2)]
+                out = out + close_rep_dist*densityZ(z_ij, U[i], U[j])/(np.std(U[i])+np.std(U[j]))
+    return out
+
+def Sep(U):
+    out = 0
+    for i in range(len(U)):
+        for j in range(len(U)):
+            if i != j:
+                close_rep_dist, x, y = closeRepCal(U[i], U[j])
+                out = out + close_rep_dist
+    return out
+
+def CDbw(U):
+    return Intra_den(U)*Sep(U)
