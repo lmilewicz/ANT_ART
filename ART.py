35b07804899e92ab2a688b539af2c1c79ec6ff6d
Maksymilian Pasek <maksymilian.pasek@nauto.com>
2019-11-05 10:55:37 +0100

    Added initial ART

---
+++ ART.py
@@ -0,0 +1,12 @@
+from ART2 import ART2
+
+class BinaryMemebershipMatrix:
+    def __init__(self, objects, labels):
+        pass
+
+class ART:
+    def __init__(self):
+        pass
+
+    def cluster(self, bmm: BinaryMemebershipMatrix):
+        pass
