ff47fa880d389e3691416a1171cd12300fb03e44
Maksymilian Pasek <maksymilian.pasek@nauto.com>
2020-01-15 10:41:19 +0100

    Implementation fixes

--- ant.py
+++ ant.py
@@ -2,13 +2,7 @@ import numpy as np
 from sklearn import datasets
 from scipy.spatial.distance import cdist
 from tqdm.auto import tqdm
-# import holoviews as hv
-# from holoviews import dim
-# import param
-# import panel as pn
-# from bokeh.plotting import show
-# from bokeh.layouts import column
-# hv.extension('bokeh')
+
 
 def ant_cluster(data, *, n, m, s, v, v_max, a, b, space_scale=1.0):
     if not callable(v):
@@ -28,12 +22,13 @@ def ant_cluster(data, *, n, m, s, v, v_max, a, b, space_scale=1.0):
     for iteration_idx, iteration in enumerate(tqdm(range(m))):
         stats = {'iteration': iteration_idx}
         Y = cdist(obj_positions, obj_positions, 'euclidean')
+        np.fill_diagonal(Y, np.inf)
 
         sim = np.apply_along_axis(lambda e: np.sum(e * data, axis=1)/(np.sqrt(np.sum(e**2))*data_sq), 1, data)
         d = 1 - sim
 
         f_ = 1 - d/(a*(1 + ((v_() - 1)/v_max)))
-        f = np.maximum(1/s**2 * (np.sum(np.where(Y < s, f_, 0), axis=1) - 1), 0)
+        f = np.maximum(1/s**2 * np.sum(np.where(Y < s, f_, 0), axis=1), 0)
 
         drop_p = sigmoid(f)
         movement = np.random.random(obj_positions.shape) - 0.5
@@ -66,49 +61,3 @@ def ant_cluster(data, *, n, m, s, v, v_max, a, b, space_scale=1.0):
         obj_positions[loaded] += movement[loaded]
         if iteration_idx % (m // 10) == 0 or True:
             yield obj_positions, stats, 1-drop_p
-
-    # yield obj_positions, None,
-
-
-# if __name__ == '__main__':
-#     np.random.seed(6)
-#     data = np.array(
-#         [
-#             np.random.randint(100, size=5),
-#             np.random.randint(100, size=5),
-#             np.random.randint(100, size=5),
-#             np.random.randint(100, size=5),
-#             np.random.randint(100, size=5),
-#             np.random.randint(100, size=5),
-#             np.random.randint(100, size=5),
-#         ]
-#     )
-#     iris = datasets.load_iris()
-#     data = iris['data'][:, :2]
-#     data = np.append(data, iris['target'][:, None]+1, axis=1)
-#
-#     iris_plt = hv.Scatter(data, vdims=['y', 'z'])
-#     iris_plt.opts(color='z', cmap='Accent')
-#     plots = []
-#
-#     v_max = 0.85
-#     v = lambda: (np.random.random(data.shape[0]) * v_max)[:, np.newaxis]
-#     for c_data in ant_cluster(data, n=50, m=100, s=3, v=v, v_max=v_max, a=1.5, b=3.0):
-#         c_data = np.append(c_data, iris['target'][:, None]+1, axis=1)
-#
-#
-#         clustered = hv.Scatter(c_data, vdims=['y', 'z'])
-#         clustered.opts(color='z', cmap='Accent')
-#         plots.append(clustered)
-#
-#     iteration = pn.widgets.IntSlider(name='Iteration', value=0, start=0, end=len(plots))
-#
-#     @pn.depends(iteration=iteration.param.value)
-#     def load_symbol_cb(iteration):
-#         return plots[iteration]
-#
-#     dmap = hv.DynamicMap(load_symbol_cb)
-#     dashboard = pn.Row(pn.WidgetBox('## ANT', iteration))
-#     # dashboard
-#     # figs = [hv.render(p) for p in plots]
-#     # show(column(*figs))
