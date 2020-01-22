54aa9a58a2193152c3ec2a755ca67ad1e6162ab4
max-pasek <maksymilian.pasek@gmail.com>
2020-01-22 02:56:00 +0100

    Fixed bugs. Added numba for faster computing

--- ant.py
+++ ant.py
@@ -1,67 +1,69 @@
 import numpy as np
+import numba as nb
 from sklearn import datasets
-from scipy.spatial.distance import cdist
+from scipy.spatial.distance import cdist as _cdist
 from tqdm.auto import tqdm
 
 
-def ant_cluster(data, *, n, m, s, v, v_max, a, b, space_scale=1.0, limit_plane=False):
-    if not callable(v):
-        v_ = lambda: v
-    else:
-        v_ = v
+@nb.njit
+def sigmoid(x, b):
+    return 1 / (1 + np.exp(-b*x))
 
-    def sigmoid(x):
-        return 1 / (1 + np.exp(-b*x))
+@nb.njit
+def similarity(e, data, data_sqrt):
+    return np.dot(data, e)/(np.linalg.norm(e)*data_sqrt)
 
+# @nb.jit(nb.f8[:,:](nb.f8[:,:], nb.f8[:,:]), forceobj=True)
+def cdist(a, b):
+    return _cdist(a, b, 'euclidean')
+
+# @nb.jit(forceobj=True)
+def ant_cluster(data, *, n, m, s, v, v_max, a, b, space_scale=1.0, limit_plane=False, summary_freq=10):
     obj_positions = np.random.random([data.shape[0], 2]) * space_scale
     loaded = np.zeros(data.shape[:1], dtype=np.bool)
     loaded[:] = False
     loaded[np.random.choice(range(data.shape[0]), n, replace=False)] = True
 
     data_sqrt = np.sqrt(np.sum(data**2, axis=1))
+    Y = cdist(obj_positions, obj_positions)
+    np.fill_diagonal(Y, np.inf)
+    
     for iteration_idx, iteration in enumerate(tqdm(range(m))):
-        stats = {'iteration': iteration_idx}
-        Y = cdist(obj_positions, obj_positions, 'euclidean')
-        np.fill_diagonal(Y, np.inf)
-
-        sim = np.apply_along_axis(lambda e: np.dot(data, e)/(np.linalg.norm(e)*data_sqrt), 1, data)
+#         Y = cdist(obj_positions, obj_positions)
+#         np.fill_diagonal(Y, np.inf)
+        
+        sim = np.apply_along_axis(similarity, 1, data, data, data_sqrt)
         d = 1 - sim
 
-        f_ = 1 - d/(a*(1 + ((v_() - 1)/v_max)))
-        f = np.maximum(1/s**2 * np.sum(np.where(Y < s, f_, 0), axis=1), 0)
+        v_iter = v()
+        f_ = 1 - d/(a*(1 + ((v_iter - 1)/v_max)))
+        f = np.sum(np.where(Y < s, f_, 0), axis=1) / s**2
+        f[f <= 0] = 0
 
-        drop_p = sigmoid(f)
+        drop_p = sigmoid(f, b)
         movement = np.random.random(obj_positions.shape) - 0.5
 #         movement = movement / np.linalg.norm(movement, axis=0)
-        movement *= v_()
+        movement *= v_iter
 
-        # np.random.seed(3)
+#         np.random.seed(3)
 #         p = np.random.random(obj_positions.shape[0])
         p = np.random.random()
 
-        stats['dropped'] = np.count_nonzero(np.logical_and(loaded, drop_p > p))
         loaded[drop_p > p] = False
         n_free_ants = n - np.count_nonzero(loaded)
-        stats['free ants'] = n_free_ants
         pickable_objs = np.flatnonzero(np.logical_and(loaded == False, drop_p <= p))
-        stats['pickable'] = len(pickable_objs)
         picked_objs = np.random.choice(pickable_objs, min(n_free_ants, len(pickable_objs)), replace=False) if len(pickable_objs) else np.array([], dtype=bool)
-        stats['picked'] = len(picked_objs)
         loaded[picked_objs] = True
-        stats['loaded'] = np.count_nonzero(loaded)
-
-        # print(n_free_ants)
-        # print(loaded)
-        # print(movement)
-        # print(picked_objs)
-
-        stats_str = ' | '.join(['{0}: {1}'.format(k, v) for k, v in stats.items()])
-        # tqdm.write(stats_str)
 
         obj_positions[loaded] += movement[loaded]
         
         if limit_plane:
-            obj_positions = obj_positions.clip(0, space_scale)
+            obj_positions[loaded] = obj_positions[loaded].clip(0, space_scale)
+            
+        nY = cdist(obj_positions[loaded], obj_positions)
+        Y[loaded, :] = nY
+        Y[:, loaded] = nY.T
+        np.fill_diagonal(Y, np.inf)
         
-        if iteration_idx % (m // 10) == 0 or True:
-            yield obj_positions, stats, 1-drop_p
+        if iteration_idx % (m // summary_freq) == 0 or summary_freq is None:
+            yield obj_positions, 1-drop_p
